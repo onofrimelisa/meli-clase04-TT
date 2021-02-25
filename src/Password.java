f9845ba6175d99a8d58d61a7038a9ff792e7661c
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-02-25 11:18:34 -0300

    feat: add Main class and remove the main from the Password class

--- src/Password.java
+++ src/Password.java
@@ -20,23 +20,4 @@ public class Password {
         if (!matcher.matches()) throw new RuntimeException("The given password doesn't match the regex.");
         this.password = password;
     }
-
-    public static void main(String[] args) {
-        System.out.println("Testing passwords...");
-        System.out.println("");
-
-        System.out.println("Strong Passwords results: ");
-        Password validStrongPassword = new StrongPassword("validPassword1!");
-        Password invalidStrongPassword = new StrongPassword("invalidPassword");
-        System.out.println();
-
-        System.out.println("Weak Passwords results: ");
-        Password validWeakPassword = new WeakPassword("validpassword1");
-        Password invalidWeakPassword = new WeakPassword("invalidpassword");
-        System.out.println();
-
-        System.out.println("Intermediate Passwords results: ");
-        Password validIntermediatePassword = new IntermediatePassword("validPassword1");
-        Password invalidIntermediatePassword = new IntermediatePassword("invalidpassword1");
-    }
 }
